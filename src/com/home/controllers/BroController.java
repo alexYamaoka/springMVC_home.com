d4af55dfdc7afc68d87538d521cad9b4c8e78b29
alexYamaoka <ryamaoka1@gmail.com>
2020-09-11 13:12:59 -0700

    fixed error in view resolver

--- src/com/home/controllers/BroController.java
+++ src/com/home/controllers/BroController.java
@@ -3,6 +3,7 @@ package com.home.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @Controller
@@ -11,6 +12,9 @@ public class BroController {
 	//@ResponseBody
 	@RequestMapping("/cricketBat")
 	public String giveCricketBat() {
-		return "/WEB-INF/view/MRFCricketbat.jsp";
+		return "MRFCricketbat";
 	}
+	
+	
+	
 }
