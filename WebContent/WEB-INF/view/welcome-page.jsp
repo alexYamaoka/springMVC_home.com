82f9a8f8aab4149859544a22b7754e42dc7ced86
alexYamaoka <ryamaoka1@gmail.com>
2020-09-11 18:41:30 -0700

    used Model interface to send data to view page

--- WebContent/WEB-INF/view/welcome-page.jsp
+++ WebContent/WEB-INF/view/welcome-page.jsp
@@ -4,9 +4,11 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Welcome</title>
 </head>
 <body>
 	<h1>Mom's Cafe</h1>
+	<hr>
+	My Name is: ${myNameValue}
 </body>
 </html>
--- src/com/home/mycafe/controllers/MyCafeControllers.java
+++ src/com/home/mycafe/controllers/MyCafeControllers.java
@@ -1,8 +1,20 @@
 package com.home.mycafe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyCafeControllers {
+	
+	@RequestMapping("/cafe")
+	public String showWelcomePage(Model model) {
+		
+		// sending data to view/jsp page
+		String myName = "Alex";
+		model.addAttribute("myNameValue", myName);
+		
+		return "welcome-page";
+	}
 	
 }
